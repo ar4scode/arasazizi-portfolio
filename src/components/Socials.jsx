5028ff68bf870f588fb07d48eec53132b58f5c2f
Ar4sCode <ar4sazizi@gmail.com>
2025-02-11 16:08:04 +0330

    Change the css code with tailwind(use group prop to solve my problem)

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,24 +5,24 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 export default function Socials() {
   return (
-    <div className="social flex w-3/4 ml-auto gap-5 mr-15 mt-8">
+    <div className="flex w-3/4 ml-auto gap-5 mr-15 mt-8">
       <code className="text-gray-500 text-xs mt-1">/socials</code>
       <ul className='flex gap-8'>
-        <li className='links'>
-          <a href="https://www.instagram.com/arasazizi__/" className='flex gap-1 text-gray-500 text-sm hover:text-black'>
-            <FontAwesomeIcon className='icon border p-1 rounded-md border-gray-300 text-gray-500 ' icon={faInstagram} />
+        <li className='group'>
+          <a href="https://www.instagram.com/arasazizi__/" className='flex gap-1 text-gray-500 text-sm hover:text-black duration-300'>
+            <FontAwesomeIcon className=' border p-1 rounded-md border-gray-300 text-gray-500 group-hover:shadow-md duration-300 ' icon={faInstagram} />
             Instagram
           </a>
         </li>
-        <li className='links'>
-          <a href="https://github.com/ar4scode" className='flex gap-1 text-gray-500 text-sm hover:text-black'>
-            <FontAwesomeIcon className='icon border p-1 rounded-md border-gray-300 text-gray-500' icon={faGithub} />
+        <li className='group'>
+          <a href="https://github.com/ar4scode" className='flex gap-1 text-gray-500 text-sm hover:text-black duration-300'>
+            <FontAwesomeIcon className='border p-1 rounded-md border-gray-300 text-gray-500 group-hover:shadow-md duration-300' icon={faGithub} />
             GitHub
           </a>
         </li>
-        <li className='links'>
-          <a href="www.linkedin.com/in/ar4scode" className='flex gap-1 text-gray-500 text-sm hover:text-black'>
-            <FontAwesomeIcon className='icon border p-1 rounded-md border-gray-300 text-gray-500' icon={faLinkedinIn} />
+        <li className='group'>
+          <a href="www.linkedin.com/in/ar4scode" className='flex gap-1 text-gray-500 text-sm hover:text-black duration-300 '>
+            <FontAwesomeIcon className='border p-1 rounded-md border-gray-300 text-gray-500 group-hover:shadow-md duration-300' icon={faLinkedinIn} />
             LinkedIn
           </a>
         </li>
