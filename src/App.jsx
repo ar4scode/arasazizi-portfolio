1ac250dd8495333bfd0b96bef8a39d6fc4369012
Ar4sCode <ar4sazizi@gmail.com>
2025-02-11 20:46:13 +0330

    Create project section and link project rep to my project section(not responsive, not tested)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Profile from './components/Profile'
 import About from './components/About'
 import Socials from './components/Socials'
+import Projects from './components/Projects'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Profile />
       <About />
       <Socials />
+      <Projects />
     </>
   )
 }
