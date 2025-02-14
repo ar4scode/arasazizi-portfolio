81bdfa92982dfa1d93d2f1c7324baee9b2b72416
Ar4sCode <ar4sazizi@gmail.com>
2025-02-14 12:00:05 +0330

    make about section responsive for mobile device

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <div className="about-container flex w-3/4 ml-auto gap-5 mr-15 mt-4">
-      <code className="text-gray-500 text-xs mt-1">/about</code>
+    <div className="about-container flex w-full gap-5 ml-5 mt-4 lg:mr15">
+      <code className="text-gray-500 text-xs mt-1 hidden md:block">/about</code>
       <div>
         <p className="text-gray-500 mb-2">
           Hey, I`m a Frontend developer based in Iran. 
