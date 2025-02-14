9f6c5a1ef293d9ba9099fba5879d6b0852dcc3cb
Ar4sCode <ar4sazizi@gmail.com>
2025-02-14 11:51:37 +0330

    modify header and profile alignment

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,13 +2,13 @@ import profileImage from "../assets/aras 4.jpg"
 
 export default function Profile() {
   return (
-    <header className="mx-auto w-full p-5 sm:px-8 max-w-screen-md grid xs:grid-cols-[75px_1fr] items-baseline gap-x-6 gap-y-4 py-0">
+    <header className="mx-auto w-full p-5 sm:px-8 max-w-screen-md grid xs:grid-cols-[75px_1fr] items-baseline gap-x-6 py-0">
       <img
         src={profileImage}
         alt="aras azizi image"
-        className="rounded-full w-28 border-4 border-gray-400"
+        className="rounded-full w-33 mt-10 border-4 border-gray-400"
       />
-      <h1 className="font-bold text-3xl pt-4">Aras Azizi</h1>
+      <h1 className="font-bold text-3xl mt-3">Aras Azizi</h1>
     </header> 
   )
 }
